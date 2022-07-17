bade507fba09ee2ed3f56709115ba6199faa6ef9
LeonChris327 <krisnalionel327@gmail.com>
2022-07-17 13:45:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>BOSSNANNY</title>
+</head>
+<body>
+    <?php include ("header.php") ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+        <img src="_assets/img/perawat.jpg" style="width: 100%; height: 400px; object-fit: cover; position: relative;">
+                <div class="center" style="position: absolute;
+  top: 25%;
+  width: 100%;
+  text-align: center;
+  font-size: 70px;">BOSSNANNY</div>
+        <!-- <p style="
+        position: absolute;
+        margin: 0 auto;
+        font-size: 70px;
+        ">BOSSNANNY</p> -->
+        
+            
+            </div>
+        </div>
+   
+    </div>
+    
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
